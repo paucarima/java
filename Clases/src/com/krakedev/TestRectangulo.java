6393ebec337b51387409ef882772e929f2ac8ca2
paucarima <paucarimap@gmail.com>
2024-03-13 12:21:37 -0500

    clase rectangulo perimetro

---
+++ Clases/src/com/krakedev/TestRectangulo.java
@@ -0,0 +1,35 @@
+package com.krakedev;
+
+public class TestRectangulo {
+
+	public static void main(String[] args) {
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo();
+
+		
+		int areaR1;
+		int areaR2;
+		double perimetro1;
+		double perimetro2;
+		
+		r1.base = 10;
+		r1.altura = 5;
+
+		r2.base = 8;
+		r2.altura = 5;
+
+		areaR1 = r1.calcularArea();
+		areaR2 = r2.calcularArea();
+		perimetro1= r1.calcularPerimetro();
+		perimetro2= r2.calcularPerimetro();
+		
+		System.out.println("Area de r1: " + areaR1);
+		System.out.println("Area de r2: " + areaR2);
+		System.out.println("Perimetro de r1: " + perimetro1);
+		System.out.println("Perimetro de r2: " + perimetro2);
+	
+
+	}
+	
+
+}
